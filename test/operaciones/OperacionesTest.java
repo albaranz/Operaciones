f46c147664c99a9e11315a8cd41a005c136a145b
alba <alba.ranz.ces>
2021-05-10 20:47:56 +0200

    test unitarios metodo rangos, resultado fallido

--- test/operaciones/OperacionesTest.java
+++ test/operaciones/OperacionesTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package operaciones;
 
 import org.junit.After;
@@ -11,51 +6,74 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
 /**
  *
  * @author albar
  */
 public class OperacionesTest {
-    
+
+    Operaciones operaciones = new Operaciones();
+
     public OperacionesTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
-     * Test of rango method, of class Operaciones.
+     * Test of rango method, of class Operaciones. 
+     * El método rango recibe 2 parámetros de tipo int y retorna un array de 
+     * tipo int. El resultado esperado tras la ejecución de dicho método es 
+     * un array con todos los valores enteros comprendidos entre los dos 
+     * parámetros (ambos incluidos) en orden ascendente (de menor a mayor). 
      */
     @Test
     public void testRango() {
         System.out.println("rango");
-        int a = 0;
-        int b = 0;
-        Operaciones instance = new Operaciones();
-        int[] expResult = null;
-        int[] result = instance.rango(a, b);
+        int a = 1;
+        int b = 5;
+        int[] result = operaciones.rango(a, b);
+
+        int[] expResult = {1, 2, 3, 4, 5};
+
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+    /**
+     * Si el primer parámetro es mayor que el segundo, debe generar igualmente 
+     * el rango en orden ascendente.
+     */
+
+    @Test
+    public void testRango2() {
+        System.out.println("rango2");
+        int a = 5;
+        int b = 1;
+        int[] result = operaciones.rango(a, b);
+
+        int[] expResult = {1, 2, 3, 4, 5};
+
+        assertArrayEquals(expResult, result);
     }
 
     /**
      * Test of sumRango method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testSumRango() {
         System.out.println("sumRango");
@@ -65,13 +83,12 @@ public class OperacionesTest {
         int expResult = 0;
         int result = instance.sumRango(a, b);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of isPar method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testIsPar() {
         System.out.println("isPar");
@@ -80,13 +97,12 @@ public class OperacionesTest {
         boolean expResult = false;
         boolean result = instance.isPar(a);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of extraerPositivos method, of class Operaciones.
      */
+    @Ignore
     @Test
     public void testExtraerPositivos() {
         System.out.println("extraerPositivos");
@@ -95,8 +111,6 @@ public class OperacionesTest {
         int[] expResult = null;
         int[] result = instance.extraerPositivos(nums);
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
 }
