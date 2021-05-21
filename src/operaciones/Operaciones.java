302894bae607a74100430ff6aa2199f654763c91
alba <alba.ces>
2021-05-21 18:50:27 +0200

    commit actualizacion

--- src/operaciones/Operaciones.java
+++ src/operaciones/Operaciones.java
@@ -1,49 +1,70 @@
 package operaciones;
 
-
 public class Operaciones {
 
-	public int[] rango (int a, int b){
-		int[] nums = new int[b-a];
-		for (int i=a; i<b; i++){
-			nums[i]=i;
-		}
-		return nums;
-	}
+    /**
+     *
+     * @param a int
+     * @param b int
+     * @return
+     */
+    public int[] rango(int a, int b) {
+        int[] nums = new int[b - a];
+        for (int i = a; i < b; i++) {
+            nums[i] = i;
+        }
+        return nums;
+    }
 
-	public int sumRango(int a, int b){
-		int sum = 0;
-		for (int i=a; i<b; i++){
-			sum+=i;
-		}
-		return sum;
-	}
+    /**
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public int sumRango(int a, int b) {
+        int sum = 0;
+        for (int i = a; i < b; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 
-	public boolean isPar(int a){
-		if(a%2==0){
-			return true;
-		}
-		return false;
-	}
+    /**
+     *
+     * @param a int
+     * @return boolean
+     */
+    public boolean isPar(int a) {
+        if (a % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
 
-	public int[] extraerPositivos(int []nums){
-		
-		int n=0;
-		for (int i=0; i<nums.length; i++){
-			if(nums[i]>0){
-				n++;
-			}
-		}
+    /**
+     *
+     * @param nums
+     * @return
+     */
+    public int[] extraerPositivos(int[] nums) {
 
-		int []positivos = new int[n];
-		int j=0;
-		for (int i=0; i<nums.length; i++){
-			if(nums[i]>0){
-				positivos[j]=nums[i];
-				j++;
-			}
-		}
-		return positivos;
-	}
-    
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                n++;
+            }
+        }
+
+        int[] positivos = new int[n];
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                positivos[j] = nums[i];
+                j++;
+            }
+        }
+        return positivos;
+    }
+
 }
